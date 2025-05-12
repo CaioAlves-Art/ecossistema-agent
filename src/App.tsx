699ee59b57eb90481065e11150dcd7c782c298c5
CaioAlves-Art <caio.alves@especialistasmg.com.br>
2025-05-12 19:43:19 -0300

    Atualiza .gitignore e estrutura final para deploy

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Ecossistema Agent – Especialista Imóveis</h1>
+      <p>Interface inicial do sistema</p>
+    </div>
+  )
+}
+
+export default App
